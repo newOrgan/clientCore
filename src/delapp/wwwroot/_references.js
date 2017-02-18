ddc34b58374a1bdd99bc9c827ec7691385fa7400
superTemir <akirabenj@hotmail.com>
2017-02-18 14:50:55 +0300

    Views add

--- src/delapp/wwwroot/_references.js
+++ src/delapp/wwwroot/_references.js
@@ -3,6 +3,7 @@
 /// <reference path="js/cartscript.js" />
 /// <reference path="js/indexanim.js" />
 /// <reference path="js/jquery.bgpos.js" />
+/// <reference path="js/jquery.maskedinput.js" />
 /// <reference path="js/jquery-3.1.1.min.js" />
 /// <reference path="js/kithcenblocks.js" />
 /// <reference path="js/site.js" />
